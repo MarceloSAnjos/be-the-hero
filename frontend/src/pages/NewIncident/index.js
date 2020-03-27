c637aadd5ecd79f423f87422f62c531f0766b509
marceloSAnjos <cellodosanjos03@gmail.com>
2020-03-27 12:34:11 -0300

    FRont-End do app

---
+++ frontend/src/pages/NewIncident/index.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi'
+
+import api from '../../services/api';
+
+import './style.css';
+
+import logoImg from '../../assets/logo.svg'
+
+export default function NewIncident() {
+
+  const ongId = localStorage.getItem('ongId');
+
+  const history = useHistory();
+
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+
+  async function handleNewIncident(e) {
+    e.preventDefault();
+
+    const data = {
+      title,
+      description,
+      value,
+    };
+
+    try {
+      await api.post('incidents', data, {
+        headers: {
+          Authorization: ongId,
+        }
+      });
+
+      history.push('/profile');
+    } catch (error) {
+      alert('Erro ao cadastrar caso');
+    }
+  }
+
+  return (
+    <div className="new-incident-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be The Hero" />
+
+          <h1>Cadastrar novo caso</h1>
+          <p>Descrea o caso detalhadamente para encontrar um herói para resolver isso.</p>
+
+          <Link className="back-link" to="/profile">
+            <FiArrowLeft size={16} color="#E02041" />
+            Voltar para home
+          </Link>
+        </section>
+        <form onSubmit={handleNewIncident}>
+          <input
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            placeholder="Titulo do caso"
+          />
+          <textarea type="email"
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+            placeholder="Descrição"
+          />
+          <input
+            value={value}
+            onChange={e => setValue(e.target.value)}
+            placeholder="Valor em reais"
+          />
+
+          <button className="button" type="submit">Cadastrar</button>
+        </form>
+      </div>
+    </div>
+  )
+}
